7ddabb0e8689b45b2c85ffc5a5d9dec57a81e8e6
ShenMian <shenmian000@outlook.com>
2020-02-07 22:40:46 +0800

    update

---
+++ Herobrine/Herobrine.cpp
@@ -0,0 +1,13 @@
+// Copyright 2019 SMS
+// License(Apache-2.0)
+
+#include "include.h"
+
+
+
+int main()
+{
+	
+
+	return 0;
+}
--- Herobrine/include.h
+++ Herobrine/include.h
@@ -7,10 +7,15 @@
 
 
 
-typedef unsigned char		uchar;
-typedef unsigned short	ushort;
-typedef unsigned int		uint;
-typedef unsigned long		ulong;
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned int   uint;
+typedef unsigned long  ulong;
+
+
+
+// C 标准库
+#include <assert.h>
 
 
 // C++ 标准库
@@ -23,6 +28,7 @@ using std::thread;
 using std::string;
 using std::to_string;
 using std::stoi;
+using std::stol;
 
 #include <vector>
 using std::vector;
@@ -36,17 +42,31 @@ using std::forward_list;
 #include <list>
 using std::list;
 
+#include <map>
+using std::map;
 
-// 定义操作系统内核宏, 用于判断操作系统内核
+#include <algorithm>
+using std::any_of;
+using std::none_of;
+
+#include <thread>
+using std::thread;
+
+#include <mutex>
+using std::mutex;
+
+
+
+// 定义操作系统宏, 用于判断操作系统类型
 #ifdef _WIN32
-	#define OS_WIN		// Windows
+	#define OS_WIN      // Windows
 	#ifdef _WIN64
 		#define OS_WIN64
 	#else
 		#define OS_WIN32
 	#endif
 #else
-	#define OS_LINUX	// Linux
+	#define   OS_LINUX  // Linux
 #endif
 
 
