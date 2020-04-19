768e2c181c65c29069f83ede39625e99fe83d547
ShenMian <shenmian000@outlook.com>
2020-04-19 09:17:35 +0800

    update

--- Console/main.cpp
+++ Console/main.cpp
@@ -5,11 +5,6 @@
 
 int main()
 {
-	Color::set(Color::Fore::cyan);
-	Color::set(Color::Mode::fore_bold);
-	Color::set(Color::Mode::underline);
-	puts("Hello HIM");
-
 	Console console;
 	console.setPrompt("him> ");
 	console.run();
